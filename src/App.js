b55295eeb53033c3da8bb332a64d28800cd128f2
sayma63 <saymasumi106353@gmail.com>
2023-01-23 20:41:42 +0600

    navbar added in the website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+
+import { Route, Routes } from 'react-router-dom';
+import { publicRoute } from "./routes/publicRoutes";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar>
+     <Routes>
+      {
+        publicRoute.map(({path,Component},index)=><Route key={index} path={path} element={<Component/>}></Route>)
+      }
+     {/* <Route path='/' element={<Home></Home>}></Route>
+     <Route path='login' element={<Login></Login>}></Route>
+     <Route path='contact' element={<Contact></Contact>}></Route>
+     <Route path='products' element={<Products></Products>}></Route>
+     <Route path ='about' element={<About></About>}></Route> */}
+
+      
+
+
+
+ 
+
+
+     {/* <Route path='*' element={<NotFound></NotFound>}></Route> */}
+
+
+       </Routes>
+
+     </Navbar>
     </div>
   );
 }
---
+++ src/routes/publicRoutes.js
@@ -0,0 +1,16 @@
+import Home from "../pages/Home"
+import Contact from "../pages/Contact"
+import Login from "../pages/Login"
+import Products from "../pages/Products"
+import About from "../pages/About";
+
+
+
+export const publicRoute=[
+    {path:'/', name:"Home", Component:Home},
+    {path:'/about', name:"About", Component:About},
+    {path:'/products', name:"Products", Component:Products},
+    {path:'/contact', name:"Contact", Component:Contact},
+    {path:'/login', name:"Login", Component:Login},
+  ];
+   
