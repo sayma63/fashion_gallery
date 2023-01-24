84e0e0077e9e995a15a493eabc861a9676180b5f
sayma63 <saymasumi106353@gmail.com>
2023-01-24 12:05:48 +0600

    AOS animation added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
         <div class="hero h-[75vh] w-4/5 mx-auto bg-accent mt-16 ">
   <div class="hero-content flex-col lg:flex-row ">
   <div>
-      <h1 class="text-5xl font-bold">Best Collections </h1>
-      <h1 class="text-5xl font-bold">for Women's </h1>
+      <h1 data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200" class="text-5xl font-bold">Best Collections </h1>
+      <h1 data-aos="fade-right" data-aos-duration="900" data-aos-delay="400" class="text-5xl font-bold">for Women's </h1>
 
-      <p class="py-6 max-w-2xl font-medium">Fashion Gallery  is the best casual and traditional ladies collection selling online shopping sites for women.It always serves customers with high quality products.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <p data-aos="fade-right" data-aos-duration="800" data-aos-delay="600" class="py-6 max-w-2xl font-medium">Fashion Gallery  is the best casual and traditional ladies collection selling online shopping sites for women.It always serves customers with high quality products.</p>
+      <button data-aos="zoom-in"  data-aos-delay="1300" class="btn btn-primary">Get Started</button>
     </div>
     <div className='h-[60vh]  '>
     <img src={shoppingGirl} class="h-[400px]  w-[50vh] mt-[-30px]" alt='' />
